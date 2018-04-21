310a1355868e3e341e90e905eaf4f4f0d3aaf9dd
hce1g16 <36552418+hce1g16@users.noreply.github.com>
2018-04-21 23:27:51 +0100

    FortunaSoduko 1.0

---
+++ _build/Sudoku.d
@@ -0,0 +1,10 @@
+_build/Sudoku.o: Sudoku.c lcd/lcd.h lcd/svgrgb565.h ruota/ruota.h \
+  themes.h
+
+lcd/lcd.h:
+
+lcd/svgrgb565.h:
+
+ruota/ruota.h:
+
+themes.h:
---
+++ themes.h
@@ -0,0 +1,11 @@
+
+struct Theme{
+  uint16_t background;
+  uint16_t cell_frame;
+  uint16_t cell_background;
+  uint16_t editable_numbers;
+  uint16_t noneditable_numbers;
+  uint16_t cursor_border;
+};
+
+#define defaulttheme {BLACK      , YELLOW     , WHITE_SMOKE, BLACK, RED, DARK_GREEN}
